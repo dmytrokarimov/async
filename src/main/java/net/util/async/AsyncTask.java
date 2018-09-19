6c1e58843c87c28b8dd1d6cfc36f6b005878253c
dmytro_karimov@live.ru <dmytro_karimov@live.ru>
2018-09-19 23:04:46 +0300

    Added AsyncTask

---
+++ src/main/java/net/util/async/AsyncTask.java
@@ -0,0 +1,41 @@
+package net.util.async;
+
+import java.util.function.Supplier;
+
+public class AsyncTask<T> extends Thread{
+	
+	private T result;
+	
+	private Exception error;
+	
+	private final Supplier<T> job;
+	
+	private AsyncTask(Supplier<T> job) {
+		setDaemon(true);
+		this.job = job;
+	}
+	
+	public static <T> AsyncTask<T> of(Supplier<T> job) {
+		return new AsyncTask<>(job);
+	}
+	
+	@Override
+	public void run() {
+		try {
+			result = job.get();
+		} catch (Exception e) {
+			error = e;
+		}
+	}
+	
+	public T getResult() throws Exception {
+		if (error != null) {
+			throw error;
+		}
+		return result;
+	}
+	
+	public Exception getError() {
+		return error;
+	}
+}
